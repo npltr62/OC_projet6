7032a8e19f8fe038133a5bdfef118ed794c91125
npltr62 <nico.pe62@gmail.com>
2022-02-05 01:37:26 +0100

    maj commandes

--- Case1.py
+++ Case1.py
@@ -1,17 +1,30 @@
+import os
 import paramiko
 import fnct
 import logging
 import yaml #import librairie yaml pour le fichier conf
-with open('C:\\Users/alouette\\Documents\\p6\\OC_projet6\\config.yaml') as f:
+with open('config.yaml') as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
 user= config['vms']['vm1']['user']
 host= config['vms']['vm1']['host']
 pwd= config['vms']['vm1']['pwd']
 logging.info('start')
 package= fnct.distrib(host,user,pwd)
-cmd1= f'{package} update'
-cmd2= f'{package} install -y wget apache2 php php-pdo php-mysql php-zip php-gd php-mbstring php-curl php-xml php-pear php-bcmath mariadb-server wordpress'
-fnct.ssh(host,user,pwd, cmd1)
-fnct.ssh(host,user,pwd, cmd2)
-fnct.upload_file(host, user, pwd,'C:\\Users/alouette\\Documents\\p6\\OC_projet6\\\wp_conf\wp.conf', '/etc/apache2/sites-available/wp.conf')
-fnct.upload_file(host, user, pwd,'C:\\Users/alouette\\Documents\\p6\\OC_projet6\\\wp_conf\config-myblog.example.com.php', '/etc/apache2/sites-available/wp.conf')
+cmd1= f'sudo {package} update && {package} full-upgrade -y'
+cmd2= f'sudo {package} install apache2 mariadb-server mariadb-client php libapache2-mod-php php-cli php-mysql php-zip php-curl php-xml wget -y'
+cmd3='sudo mysql -u root < wp.sql'
+cmd4='sudo wget -c https://wordpress.org/latest.tar.gz && sudo tar -xzf  latest.tar.gz -C /var/www/html'
+cmd5='sudo rm /var/www/html/latest.tar.gz'
+cmd5='sudo chown -R www-data:www-data /var/www/html/wordpress'
+cmd5='sudo cp /wp_conf/wp-config.php /var/www/html/wordpress/ '
+
+# fnct.ssh(host,user,pwd, cmd1)
+# fnct.ssh(host,user,pwd, cmd2)
+# fnct.ssh(host,user,pwd, cmd3)
+# fnct.upload_file(host, user, pwd,'C:\\Users/alouette\\Documents\\p6\\OC_projet6\\wp_conf\\wp.conf', '/etc/apache2/sites-available/wp.conf')
+fnct.upload_file(host, user, pwd,'C:\\Users/alouette\\Documents\\p6\\OC_projet6\\wp_conf\\wp-config.php', f'/home/{user}/wp-config.php')
+# fnct.upload_file(host, user, pwd,'C:\\Users/alouette\\Documents\\p6\\OC_projet6\\wp_conf\\wp.sql', f'/home/{user}/wp.sql')
+# def install_function():
+#     os.system('sudo apt-get update')
+
+# install_function()
