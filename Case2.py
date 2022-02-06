ac2b31ae630fe3d308fcdb9a67c108c2ab2792cc
npltr62 <nico.pe62@gmail.com>
2022-02-06 21:35:19 +0100

    maj cmd

--- Case2.py
+++ Case2.py
@@ -1,2 +1,16 @@
-'sudo mysqldump -u root > dump.sql'
-'lftp ftp://identifiant:mot_de_passe@site_de_connexion -e "put -O /repertoire/fichier/distant/ /chemin/local/du/fichier; quit"'
+import fnct
+import logging
+import yaml #import librairie yaml pour le fichier conf
+with open('config.yaml') as f:
+    config = yaml.load(f, Loader=yaml.FullLoader)
+user= config['vms']['vm2']['user']
+host= config['vms']['vm2']['host']
+pwd= config['vms']['vm2']['pwd']
+package= fnct.distrib()
+cmd1= f'sudo {package} install lftp -y'
+cmd1='sudo mysqldump -u root wordpress> dump.sql'
+cmd2=f'lftp sftp://{user}:{pwd}@{host} -e "put ~/dump.sql; quit"'
+logging.info('start dump')
+fnct.run(cmd1)
+logging.info('start')
+fnct.run(cmd2)
--- Case3.py
+++ Case3.py
@@ -1,8 +1,10 @@
 import paramiko
-import distrib
+import fnct
 import yaml #import librairie yaml pour le fichier conf
 import logging #import librairie logging permettant de logger les étapes du script
-ssh_client=paramiko.SSHClient()
-ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh_client.connect(hostname=host, username=user, password=pwd, port=22)
-stdin,stdout,stderr=ssh_client.exec_command('apt-get purge wordpress')
+cmd1='sudo rm /var/www/html/wordpress'
+cmd2='sudo mysql -u root < drop_wp.sql'
+cmd3='sudo wget -c https://wordpress.org/latest.tar.gz && sudo tar -xzf  latest.tar.gz -C /var/www/html'
+fnct.run(cmd1)
+fnct.run(cmd2)
+fnct.run(cmd3)
