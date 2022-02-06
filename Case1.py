5c18dcb75eb86294839b34234a6ecd7ea482c6c0
npltr62 <nico.pe62@gmail.com>
2022-02-06 21:35:02 +0100

    maj cmd

--- Case1.py
+++ Case1.py
@@ -1,5 +1,3 @@
-import os
-import paramiko
 import fnct
 import logging
 import yaml #import librairie yaml pour le fichier conf
@@ -9,22 +7,18 @@ user= config['vms']['vm1']['user']
 host= config['vms']['vm1']['host']
 pwd= config['vms']['vm1']['pwd']
 logging.info('start')
-package= fnct.distrib(host,user,pwd)
+package= fnct.distrib()
 cmd1= f'sudo {package} update && {package} full-upgrade -y'
 cmd2= f'sudo {package} install apache2 mariadb-server mariadb-client php libapache2-mod-php php-cli php-mysql php-zip php-curl php-xml wget -y'
 cmd3='sudo mysql -u root < wp.sql'
 cmd4='sudo wget -c https://wordpress.org/latest.tar.gz && sudo tar -xzf  latest.tar.gz -C /var/www/html'
 cmd5='sudo rm /var/www/html/latest.tar.gz'
-cmd5='sudo chown -R www-data:www-data /var/www/html/wordpress'
-cmd5='sudo cp /wp_conf/wp-config.php /var/www/html/wordpress/ '
-
-# fnct.ssh(host,user,pwd, cmd1)
-# fnct.ssh(host,user,pwd, cmd2)
-# fnct.ssh(host,user,pwd, cmd3)
-# fnct.upload_file(host, user, pwd,'C:\\Users/alouette\\Documents\\p6\\OC_projet6\\wp_conf\\wp.conf', '/etc/apache2/sites-available/wp.conf')
-fnct.upload_file(host, user, pwd,'C:\\Users/alouette\\Documents\\p6\\OC_projet6\\wp_conf\\wp-config.php', f'/home/{user}/wp-config.php')
-# fnct.upload_file(host, user, pwd,'C:\\Users/alouette\\Documents\\p6\\OC_projet6\\wp_conf\\wp.sql', f'/home/{user}/wp.sql')
-# def install_function():
-#     os.system('sudo apt-get update')
-
-# install_function()
+cmd6='sudo chown -R www-data:www-data /var/www/html/wordpress'
+cmd7='sudo cp /wp_conf/wp-config.php /var/www/html/wordpress/ '
+fnct.run(cmd1)
+fnct.run(cmd2)
+fnct.run(cmd3)
+fnct.run(cmd4)
+fnct.run(cmd5)
+fnct.run(cmd6)
+fnct.run(cmd7)
