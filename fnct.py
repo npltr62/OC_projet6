bce525415d80f44b51be14894e974c2f2caa9871
npltr62 <nico.pe62@gmail.com>
2022-02-12 01:30:58 +0100

    ajout fonctions

--- fnct.py
+++ fnct.py
@@ -1,6 +1,10 @@
-import os
+import subprocess
 import distro
+import logging #import librairie logging permettant de logger les étapes du script
 def distrib(): #détermination de la distribution linux
     return distro.name()
 def run(cmd):
-    os.system(cmd)
+    try:
+        subprocess.run(cmd, shell=True, check=True, timeout=10, capture_output=True)
+    except subprocess.CalledProcessError as e:
+        logging.error(e)
