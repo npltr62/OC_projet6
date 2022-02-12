74348ad3ffd0cd203aa44238e0fb41ab13d68b87
npltr62 <nico.pe62@gmail.com>
2022-02-12 01:29:49 +0100

    run sql

--- Case3.py
+++ Case3.py
@@ -1,9 +1,8 @@
-import paramiko
 import fnct
 import yaml #import librairie yaml pour le fichier conf
 import logging #import librairie logging permettant de logger les étapes du script
 cmd1='sudo rm /var/www/html/wordpress'
-cmd2='sudo mysql -u root < drop_wp.sql'
+cmd2='sudo mysql -u root < wp_rm.sql'
 cmd3='sudo wget -c https://wordpress.org/latest.tar.gz && sudo tar -xzf  latest.tar.gz -C /var/www/html'
 fnct.run(cmd1)
 fnct.run(cmd2)
