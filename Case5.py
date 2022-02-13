1af59423caf581ff23489d42c2fe309d2b31badb
npltr62 <nico.pe62@gmail.com>
2022-02-13 23:50:55 +0100

    crontab

---
+++ Case5.py
@@ -0,0 +1,17 @@
+import fnct
+import logging
+import yaml #import librairie yaml pour le fichier conf
+with open('config.yaml') as f:
+    config = yaml.load(f, Loader=yaml.FullLoader)
+user= config['vm']['user']
+host= config['vm']['host']
+pwd= config['vm']['pwd']
+package= fnct.distrib()
+cmd1= f'sudo {package} install lftp -y'
+cmd2=f'lftp sftp://{user}:{pwd}@{host} -e "get /backups/dump.sql; quit"'
+cmd3='sudo mysql -u root < dump.sql'
+fnct.run(cmd1)
+logging.info('start download from ftp server')
+fnct.run(cmd2)
+logging.info('start dump')
+fnct.run(cmd3)
