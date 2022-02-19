0c564bdd9c8b79fdf3130ba62dc860f4774a871f
npltr62 <nico.pe62@gmail.com>
2022-02-19 16:28:39 +0100

    modfully

--- fnct.py
+++ fnct.py
@@ -2,7 +2,11 @@ import subprocess
 import distro
 import logging #import librairie logging permettant de logger les étapes du script
 def distrib(): #détermination de la distribution linux
-    return distro.name()
+    yum = ['fedora', 'centos','redhat']
+    if distro.id() in yum:
+        return 'yum'
+    else: 
+        return 'apt'
 def run(cmd):
     try:
         subprocess.run(cmd, shell=True, check=True, timeout=10, capture_output=True)
