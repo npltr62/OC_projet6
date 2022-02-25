3e6ce0069c6878f6c192345789c31207387ea9d6
npltr62 <nico.pe62@gmail.com>
2022-02-25 11:14:37 +0100

    valid cron

--- cron.sh
+++ cron.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-mkdir -p ~/backup; cp -R /var/www/html/wordpress /home/$1/backup/; sudo mysqldump -u root wordpress > $4_dump.sql && lftp sftp://$1:$2@$3 -e "mkdir -f backup; put -E -O ~/backup $4_dump.sql; quit") | sort - | uniq - | crontab - && rm $4_dump.sql;
-sudo tar -cvf /home/$1/$4_backup.tar.gz ~/backup;
+mkdir -p ~/backup; cp -R /var/www/html/wordpress /home/$1/backup/; sudo mysqldump -u root wordpress > /home/$1/backup/$4_dump.sql && sudo tar -cvf /home/$1/$4_backup.tar.gz ~/backup;&& lftp sftp://$1:$2@$3 -e "mkdir -f backup; put -E -O ~/backup /home/$1/$4_backup.tar.gz; quit") | sort - | uniq - | crontab -;
