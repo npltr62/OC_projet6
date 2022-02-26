48f3c8d770109ce0ee81f4685f9c11c7ce99fbe0
npltr62 <nico.pe62@gmail.com>
2022-02-26 14:53:58 +0100

    modif Case1

--- Case1.py
+++ Case1.py
@@ -10,15 +10,13 @@ logging.info('start')
 package= fnct.distrib()
 cmd1= f'sudo {package} update && {package} full-upgrade -y'
 cmd2= f'sudo {package} install apache2 mariadb-server mariadb-client php libapache2-mod-php php-cli php-mysql php-zip php-curl php-xml wget -y'
-cmd3='sudo mysql -u root < wp.sql'
+cmd3='sudo mysql -u root < wp_conf/wp.sql'
 cmd4='sudo wget -c https://wordpress.org/latest.tar.gz && sudo tar -xzf  latest.tar.gz -C /var/www/html'
 cmd5='sudo rm /var/www/html/latest.tar.gz'
-cmd6='sudo chown -R www-data:www-data /var/www/html/wordpress'
-cmd7='sudo cp /wp_conf/wp-config.php /var/www/html/wordpress/ '
+cmd6='sudo cp wp_conf/wp-config.php /var/www/html/wordpress/ && sudo chown -R www-data:www-data /var/www/html/wordpress'
 fnct.run(cmd1)
 fnct.run(cmd2)
 fnct.run(cmd3)
 fnct.run(cmd4)
 fnct.run(cmd5)
 fnct.run(cmd6)
-fnct.run(cmd7)
