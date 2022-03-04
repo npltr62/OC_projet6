95b939834353f36544a430ecfe5dbf6c9de26981
npltr62 <nico.pe62@gmail.com>
2022-03-05 00:05:44 +0100

    modif message

--- Case5.py
+++ Case5.py
@@ -14,5 +14,5 @@ datestr = strftime('%Y_%m_%d') #get current date in specific format
 package= fnct.distrib() #detect witch installing package should be used
 cmd1= f'sudo {package} install lftp -y; mkdir -p ~/.cron; cp backup_upload.sh ~/.cron/ && (crontab -l; echo "{env.syntaxe} sh /home/{env.user}/.cron/backup_upload.sh {env.user} {env.pwd} {env.host} {datestr}") | sort - | uniq - | crontab -'
 logging.info('start set cronjob')
-print('start set cronjob')
+print(f'Set cronjob: {env.syntaxe}')
 fnct.run(cmd1) #set cronjob in crontab
