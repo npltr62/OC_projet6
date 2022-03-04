55f320cc5b2975e9051fb5a72a8438a03b22ad37
npltr62 <nico.pe62@gmail.com>
2022-03-05 00:46:41 +0100

    maj comm

--- script.py
+++ script.py
@@ -1,33 +1,44 @@
-"""Prints information about an FMI observation station to the screen.
+"""
+/***************************************************************************
+    Wordpress_actions
+    
+    GitHub repo : https://github.com/npltr62/OC_projet6
+                              -------------------
+        Date                  : 01.03.2022
+        Author                : @npltr62
+ ***************************************************************************/
 
-Usage:
-    ./stationinfo.py
-
-Author:
-    @npltr62 - 01.03.2022
+/***************************************************************************
+ *                                                                         *
+ * This program is usefull to automate some actions of your wordpress site *
+ *               Initialize a Wordpress configuration                      *
+ *               Backup a site and upload to remote server                 *
+ *               Hard reset a Wordpress configuration                      *
+ *               Download a backup from a remote server and restore it     *
+ *               Schedule a backup job                                     *
+ ***************************************************************************/
 """
 import logging #import librairie logging permettant de logger les étapes du script
-from time import strftime
-import fnct
-fnct.run('mkdir -p logs')
-datestr = strftime('[%d_%m_%Y_%T]')
-datebis = strftime('%Y_%m_%d')
-logfileinfo = f'./logs/{datebis}.log'
-logformat = '%(asctime)s %(levelname)s %(message)s'
-logging.basicConfig(filename=logfileinfo, filemode='w', level=logging.INFO, format=logformat)
+from time import strftime #call time module
+import fnct #call functions
+fnct.run('mkdir -p logs') #create logs folder
+date = strftime('%Y_%m_%d') #create logs folder
+logfileinfo = f'./logs/{date}.log' #logfile name
+logformat = '%(asctime)s %(levelname)s %(message)s' #logformat
+logging.basicConfig(filename=logfileinfo, filemode='w', level=logging.INFO, format=logformat) #log config
 logging.info('start script')
 def main():
-    package= fnct.distrib()
-    cmd1= f'sudo {package} update'
-    cmd2= f'sudo {package} install python3-distro python3-yaml -y'
-    fnct.run(cmd1)
-    fnct.run(cmd2)
+    package= fnct.distrib() #detect witch installing package should be used
+    cmd1= f'sudo {package} update' 
+    cmd2= f'sudo {package} install python3-distro python3-yaml -y' 
+    fnct.run(cmd1) #check updates
+    fnct.run(cmd2) #install distro and yaml packages
     print("""
         1.init wordpress
         2.backup and upload ftp server
         3.hardreset wordpress
         4.restore last backup
-        5.run crontab
+        5.run cronjob
         """)
     choice= input("What would you like to do? ")
     if choice=="1":
