73a56e4427b874cc9b018da2a29d64fd5e17f82e
npltr62 <nico.pe62@gmail.com>
2022-03-05 00:54:47 +0100

    add rm backup

--- Case4.py
+++ Case4.py
@@ -13,7 +13,7 @@ import env #call variables
 import logging #call logging module
 package= fnct.distrib() #detect witch installing package should be used
 cmd1= f'sudo {package} install lftp -y'
-cmd2=f'lftp sftp://{env.user}:{env.pwd}@{env.host} -e "get ~/backup/{env.datestr}_backup.tar.gz ; quit" && sudo tar -xf {env.datestr}_backup.tar.gz -C /var/www/html --strip-components=3'
+cmd2=f'lftp sftp://{env.user}:{env.pwd}@{env.host} -e "get ~/backup/{env.datestr}_backup.tar.gz ; quit" && sudo tar -xf {env.datestr}_backup.tar.gz -C /var/www/html --strip-components=3 && sudo rm {env.datestr}_backup.tar.gz'
 cmd3=f'sudo mysql -u root wordpress < /var/www/html/{env.datestr}_dump.sql && sudo rm -f /var/www/html/{env.datestr}_dump.sql'
 cmd4='sudo chown -R root:root /var/www/html/wordpress'
 fnct.run(cmd1) #install ltfp package
