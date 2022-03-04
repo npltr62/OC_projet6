60574b9f168760840fdb72303784787fee9bdff5
npltr62 <nico.pe62@gmail.com>
2022-03-04 18:33:11 +0100

    drop yaml mod

--- fnct.py
+++ fnct.py
@@ -8,14 +8,6 @@ Author:
 import subprocess #import subprocess module
 import distro #import distro module
 import logging #import logging module
-import yaml #call yaml module
-with open('config.yaml') as f: #parsing yaml config file
-    config = yaml.load(f, Loader=yaml.FullLoader)
-user= config['vm']['user'] #get value from 'user' key
-host= config['vm']['host'] #get value from 'host' key
-pwd= config['vm']['pwd'] #get value from 'pwd' key
-datestr = config['latest_dump_file']['date'] #get value from 'date' key
-syntaxe= config['cron']['syntaxe'] #get value from 'syntaxe' key
 def distrib(): #check witch linux distribution is used and return the packaging name
     yum = ['fedora', 'centos','redhat']
     if distro.id() in yum:
