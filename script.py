c3824d92a2120c88d587662385efc9c52f1fba5f
npltr62 <nico.pe62@gmail.com>
2022-03-05 01:40:47 +0100

    end script and comm

--- script.py
+++ script.py
@@ -21,11 +21,12 @@ import logging #import librairie logging permettant de logger les étapes du scr
 from time import strftime #call time module
 import fnct #call functions
 fnct.run('mkdir -p logs') #create logs folder
+time = strftime('%T')
 date = strftime('%Y_%m_%d') #create logs folder
 logfileinfo = f'./logs/{date}.log' #logfile name
 logformat = '%(asctime)s %(levelname)s %(message)s' #logformat
 logging.basicConfig(filename=logfileinfo, level=logging.INFO, format=logformat) #log config
-logging.info('start script')
+logging.info(f'*----------START SCRIPT AT {time}----------*')
 def main():
     package= fnct.distrib() #detect witch installing package should be used
     cmd1= f'sudo {package} update' 
@@ -33,33 +34,38 @@ def main():
     fnct.run(cmd1) #check updates
     fnct.run(cmd2) #install distro and yaml packages
     print("""
-        1.init wordpress
-        2.backup and upload ftp server
-        3.hardreset wordpress
-        4.restore last backup
-        5.run cronjob
+        1.Init wordpress
+        2.Backup and upload to ftp server
+        3.Hardreset wordpress
+        4.Restore last backup
+        5.Schedule a backup job 
         """)
     choice= input("What would you like to do? ")
     if choice=="1":
         logging.info('***init wordpress***')
         print("\n Launch install")
         exec(open("Case1.py").read())
+        logging.info(f'*----------END SCRIPT AT {time}----------*')
     elif choice=="2":
         logging.info('***backup and upload ftp server***')
         print("\n Launch backup")        
         exec(open("Case2.py").read())
+        logging.info('*----------END SCRIPT----------*')
     elif choice=="3":
         logging.info('***hardreset wordpress***')
         print("\n Launch reset")
         exec(open("Case3.py").read())                
+        logging.info('*----------END SCRIPT----------*')
     elif choice=="4":
         logging.info('***restore last backup***')
         print("\n Launch restore") 
-        exec(open("Case4.py").read())       
+        exec(open("Case4.py").read())    
+        logging.info('*----------END SCRIPT----------*')           
     elif choice=="5":
         logging.info('***run crontab***')
         print("\n Launch cronjob")            
         exec(open("Case5.py").read())       
+        logging.info('*----------END SCRIPT----------*')
     else:
         print("\n Choose an option, try again!")
 main()
