d035b2d72e56d4cfab81fab4e2efced217cdf39f
npltr62 <nico.pe62@gmail.com>
2022-03-25 21:52:54 +0100

    add find

--- backup_upload.sh
+++ backup_upload.sh
@@ -7,4 +7,4 @@
     # 3. Dump de la base de donnée wordpress dans répertoire backup
     # 4. Mise en archive tar.gz du répertoire backup
     # 5. Upload de l'archive tar.gz vers le serveur ftp
-mkdir -p ~/backup; cp -R /var/www/html/wordpress /home/$1/backup/; sudo mysqldump -u root wordpress > /home/$1/backup/$4_dump.sql && sudo tar -cvf /home/$1/$4_backup.tar.gz ~/backup && lftp sftp://$1:$2@$3 -e "mkdir -f backup; put -E -O ~/backup /home/$1/$4_backup.tar.gz; quit";
+mkdir -p ~/backup; cp -R /var/www/html/wordpress /home/$1/backup/; sudo mysqldump -u root wordpress > /home/$1/backup/$4_dump.sql && sudo tar -cvf /home/$1/$4_backup.tar.gz ~/backup && lftp sftp://$1:$2@$3 -e "mkdir -f backup; find ~/backup/* -type f -mmin +2 -exec rm  '{}';put -E -O ~/backup /home/$1/$4_backup.tar.gz; quit";
